e64ef1fd04d34e24ed437ad41694449073324b30
Alex <alex@alexseifert.com>
2018-02-11 13:57:55 +0100

    Added Matomo tracking

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const router = express.Router();
 //const cache = require('apicache').middleware;
 
+const track = require('../lib/matomo');
+
 router.get('/', (req, res) => {
   const pageTitle = 'Homepage';
+
+  track(req, pageTitle);
 
   res.render('home/index.njk', {
     pageTitle: pageTitle,
