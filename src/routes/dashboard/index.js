c157a01d634e70eeb1be4c5c2694cedcbb46b20c
Alex <alex@alexseifert.com>
2018-02-16 16:47:07 +0100

    Added the latest 5 exhibitions to the dashboard homepage

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -4,13 +4,26 @@ const express = require('express');
 const router = express.Router();
 const cache = require('apicache').middleware;
 
-router.get('/', (req, res) => {
-  const pageTitle = 'Dashboard';
+const errorHandling = require('../../lib/errorHandling');
 
-  res.render('dashboard/dashboard.njk', {
-    pageTitle: pageTitle,
-    pageId: pageTitle.toLowerCase()
-  });
+const Exhibition = require('../../models/Exhibition');
+
+router.get('/', async (req, res) => {
+  const pageTitle = 'Dashboard';
+  const itemLimit = 5;
+
+  try {
+    const exhibitions = await Exhibition.find().sort({updatedAt: 'desc'}).limit(itemLimit).exec();
+
+    res.render('dashboard/index.njk', {
+      pageTitle: pageTitle,
+      pageId: pageTitle.toLowerCase(),
+      exhibitions: exhibitions,
+    });
+  }
+  catch(error) {
+    errorHandling.returnError(error, res, req);
+  }
 });
 
 router.get('/markdown-guide', cache('1 day'), (req, res) => {
