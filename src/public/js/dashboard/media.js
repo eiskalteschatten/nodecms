5de8cd8aaedb646ad82dfb4467634b1360f0220b
Alex <alex@alexseifert.com>
2018-05-10 22:22:40 +0200

    Files are now uploadble by drag and drop as well as by clicking on the button

--- src/public/js/dashboard/media.js
+++ src/public/js/dashboard/media.js
@@ -1,3 +1,7 @@
+var $uploadOverlay = $('.js-file-upload-overlay');
+var $fileUpload = $('#fileUpload');
+var $uploadForm = $('#uploadForm');
+
 var _dashboardMedia = {
   init: function() {
     $('.js-media-file-upload-button').click(function() {
@@ -5,24 +9,72 @@ var _dashboardMedia = {
     });
 
     var $dropArea = $('html');
-    var $uploadOverlay = $('.js-file-upload-overlay');
 
-    $dropArea.on('dragover', function() {
-      $uploadOverlay.removeClass('uk-hidden');
+    $dropArea.on('dragover', function(e) {
+      e.preventDefault();
+      _dashboardMedia.openUploadOverlay();
     });
 
-    $dropArea.on('dragleave', function() {
-      $uploadOverlay.addClass('uk-hidden');
+    $dropArea.on('dragleave', function(e) {
+      e.preventDefault();
+      _dashboardMedia.closeUploadOverlay();
     });
 
-    $dropArea.on('drop', function() {
+    $dropArea.on('drop', function(e) {
+      e.preventDefault();
+      _dashboardMedia.upload(e.originalEvent.dataTransfer.files);
+    });
 
+    $fileUpload.on('change', function() {
+      _dashboardMedia.upload();
     });
   },
 
-  upload: function() {
+  upload: function(files = null) {
+    var $body = $('body');
+    var ajaxData = new FormData($uploadForm.get(0));
 
-  }
+    if ($body.hasClass('is-uploading')) {
+      return;
+    }
+
+    $body.addClass('is-uploading');
+
+    _loader.open();
+
+    if (files) {
+      $.each(files, function(i, file) {
+        ajaxData.append($fileUpload.attr('name'), file);
+      });
+    }
+
+    $.ajax({
+      url: $uploadForm.attr('action'),
+      type: $uploadForm.attr('method'),
+      data: ajaxData,
+      dataType: 'json',
+      cache: false,
+      contentType: false,
+      processData: false
+    })
+    .done(function(id) {
+      window.location = '/dashboard/media/edit/' + id;
+    })
+    .fail(function(xhr, status, error) {
+      _loader.close();
+      _dashboardMedia.closeUploadOverlay();
+      $body.removeClass('is-uploading');
+      _messages.show('error', xhr.responseText);
+    });
+  },
+
+  openUploadOverlay: function() {
+    $uploadOverlay.removeClass('uk-hidden');
+  },
+
+  closeUploadOverlay: function() {
+    $uploadOverlay.addClass('uk-hidden');
+  },
 };
 
 $(document).ready(function() {
--- src/routes/dashboard/media.js
+++ src/routes/dashboard/media.js
@@ -2,13 +2,24 @@
 
 const express = require('express');
 const router = express.Router();
-//const path = require('path');
+const path = require('path');
+const multer = require('multer');
 const errorHandling = require('../../lib/errorHandling');
 
 const MediaFile = require('../../models/MediaFile');
 //const Categories = require('../../models/Categories');
 
-//const pathToUploadDir = path.join(__dirname, '../../public/uploads');
+const pathToUploadDir = path.join(__dirname, '../../public/uploads');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, pathToUploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({storage: storage});
 
 router.get('/', async (req, res) => {
   const pageTitle = 'Media';
@@ -31,10 +42,12 @@ router.get('/', async (req, res) => {
 });
 
 
-router.post('/', async (req, res) => {
+router.post('/', upload.array('files'), async (req, res) => {
+  //const files = req.files;
+
   try {
     const id = '';
-    res.redirect(`/dashboard/media/edit/${id}`);
+    res.send(id);
   }
   catch(error) {
     errorHandling.returnError(error, res, req);
