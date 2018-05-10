43cdcf907aaa013c00d6737f0bbe0f868ff8c4ea
Alex <alex@alexseifert.com>
2018-05-10 19:13:13 +0200

    Started media file upload

--- src/public/js/dashboard/media.js
+++ src/public/js/dashboard/media.js
@@ -1,5 +1,26 @@
 var _dashboardMedia = {
   init: function() {
+    $('.js-media-file-upload-button').click(function() {
+      $('#fileUpload').trigger('click');
+    });
+
+    var $dropArea = $('.js-media-file-drop');
+
+    $dropArea.on('dragover', function() {
+      $(this).addClass('dragging');
+    });
+
+    $dropArea.on('dragleave', function() {
+      $(this).removeClass('dragging');
+    });
+
+    $dropArea.on('drop', function() {
+
+    });
+  },
+
+  upload: function() {
+
   }
 };
 
--- src/routes/dashboard/media.js
+++ src/routes/dashboard/media.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const errorHandling = require('../../lib/errorHandling');
 
 const MediaFile = require('../../models/MediaFile');
+const Categories = require('../../models/Categories');
 
 //const pathToUploadDir = path.join(__dirname, '../../public/uploads');
 
@@ -30,17 +31,36 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/upload', (req, res) => {
+router.get('/upload', async (req, res) => {
   const pageTitle = 'Upload New File';
 
-  res.render('dashboard/media/upload.njk', {
-    pageTitle: pageTitle,
-    pageId: 'uploadNewMediaFile',
-    breadcrumbs: {
-      '/dashboard/media': 'Media',
-      '/dashboard/media/upload': pageTitle
-    }
-  });
+  try {
+    const categories = await Categories.find({}).exec();
+
+    res.render('dashboard/media/upload.njk', {
+      pageTitle: pageTitle,
+      pageId: 'uploadNewMediaFile',
+      categories: categories,
+      breadcrumbs: {
+        '/dashboard/media': 'Media',
+        '/dashboard/media/upload': pageTitle
+      }
+    });
+  }
+  catch(error) {
+    errorHandling.returnError(error, res, req);
+  }
+});
+
+
+router.post('/upload', async (req, res) => {
+  try {
+    const id = '';
+    res.redirect(`/dashboard/media/edit/${id}`);
+  }
+  catch(error) {
+    errorHandling.returnError(error, res, req);
+  }
 });
 
 
