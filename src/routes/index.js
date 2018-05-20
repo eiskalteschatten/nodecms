c746e9882134b6d374fc4d903af72790f6499eb7
Alex <alex@alexseifert.com>
2018-05-20 10:52:13 +0200

    Removed Matomo from backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,7 @@ const express = require('express');
 const router = express.Router();
 //const cache = require('apicache').middleware;
 
-const track = require('../lib/matomo');
-
 router.get('/', async (req, res) => {
-    track(req, 'Home');
-
     res.render('home/index.njk', {
         pageId: 'home'
     });
